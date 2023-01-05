5981aa77b8697224be99e6e904368fc81bf512b1
AliMostafaRadwan <omar12ali12radwan@gmail.com>
2023-01-05 20:36:06 +0200

    multi pongs

--- app/main.py
+++ app/main.py
@@ -9,8 +9,10 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    sock_cl, addr = server_socket.accept()
-    sock_cl.send(b"+PONG\r\n")
+    while True:
+        sock_cl, addr = server_socket.accept()
+        sock_cl.send(b"+PONG\r\n")
+
 
 if __name__ == "__main__":
     main()
